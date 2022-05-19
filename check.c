51836c6b53fec96adfa721ae22b8d159374cb9c2
Adedayo Adepoju <1mole.coco@gmail.com>
2022-05-19 17:46:53 +0100

    initial commit

--- check.c
+++ check.c
@@ -5,9 +5,9 @@
  *@buf: line drived fromgetline function
  *Return: 1 if cmd excuted 0 if cmd is not executed
  */
-int checker(char **cmd, char *buf)
+int checker(char **cmd, char *buffer)
 {
-	if (handle_builtin(cmd, buf))
+	if (handle_builtin(cmd, buffer))
 		return (1);
 	else if (**cmd == '/')
 	{
